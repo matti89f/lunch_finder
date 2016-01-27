79952665df12b6afcb760ce4e3176e2d1ec26364
Matti Ezzat <matti.ezzat@wlw.de>
2016-01-27 15:50:48 +0100

    add some photos

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,23 +1,23 @@
-# config/initializers/geocoder.rb
-Geocoder.configure(
+# # config/initializers/geocoder.rb
+# Geocoder.configure(
 
-  # geocoding service (see below for supported options):
-  :lookup => :yandex,
+#   # geocoding service (see below for supported options):
+#   :lookup => :yandex,
 
-  # IP address geocoding service (see below for supported options):
-  :ip_lookup => :maxmind,
+#   # IP address geocoding service (see below for supported options):
+#   :ip_lookup => :maxmind,
 
-  # to use an API key:
-  :api_key => "...",
+#   # to use an API key:
+#   :api_key => "...",
 
-  # geocoding service request timeout, in seconds (default 3):
-  :timeout => 5,
+#   # geocoding service request timeout, in seconds (default 3):
+#   :timeout => 5,
 
-  # set default units to kilometers:
-  :units => :km,
+#   # set default units to kilometers:
+#   :units => :km,
 
-  # caching (see below for details):
-  :cache => Redis.new,
-  :cache_prefix => "..."
+#   # caching (see below for details):
+#   :cache => Redis.new,
+#   :cache_prefix => "..."
 
-)
+# )
