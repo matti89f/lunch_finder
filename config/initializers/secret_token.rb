2f3b515feb550d3e4880ced4c5d26e5f65b0894c
Matti Ezzat <matti.ezzat@wlw.de>
2016-01-28 17:21:39 +0100

    add secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,5 @@
-LunchFinder::Application.config.secret_token = ENV['SECRET_TOKEN']
+LunchFinder::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
