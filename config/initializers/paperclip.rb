5f32001a632c932560c4d471a6d8a40cdbe137f8
Matti Ezzat <matti.ezzat@wlw.de>
2016-01-29 14:35:20 +0100

    fix paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'lunch-finder.s3-website.eu-central-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.eu-central-1.amazonaws.com'
